08308eb40492f34280feaa9f674b33d2dffe182b
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-04-14 13:25:13 +0800

    View engines
    Using ejs to accommodate dynamic html. Also used partials to make things look efficient

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 const express = require('express');
 
+
 // express app
 const app = express();
+
+
+// register view engine
+app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(5000);
 
+// homepage
 app.get('/', (req, res) => {
     // res.send('<h1> homepage </h1>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
+// about page
 app.get('/about', (req, res) => {
     // res.send('<h1> homepage </h1>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
-// redirects
-
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// create blog page
+app.get('/create', (req, res) => {
+    res.render('create');
+})
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
