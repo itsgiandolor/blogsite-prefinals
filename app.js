51a2f2a3f4e5181d9bf335355acdcbd37c9acbb4
itsgiandolor <gdolor@gbox.adnu.edu.ph>
2025-04-14 19:23:10 +0800

    MongoDB
    Integrated the database with the help of mongoose

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://astraeiaaaa:astraeiaaaa@blogs.wujlxjp.mongodb.net/?retryWrites=true&w=majority&appName=blogs'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(5000))
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(5000);
-
 // middleware & static files
 app.use(express.static('public'));
 
+// routes
 // homepage
 app.get('/', (req, res) => {
     // res.send('<h1> homepage </h1>');
